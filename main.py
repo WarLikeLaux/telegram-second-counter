aa63ce244583692de59239d1d78bf712face63c8
WarLikeLaux <warlikelaux080399@mail.ru>
2023-10-30 22:10:34 +0600

    Formatted code with linter Black

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ import ptbot
 
 
 def render_progressbar(
-  total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'
+    total, iteration, prefix="", suffix="", length=30, fill="█", zfill="░"
 ):
     iteration = min(total, total - iteration)
     percent = "{0:.1f}"
     percent = percent.format(100 * (iteration / float(total)))
     filled_length = int(length * iteration // total)
     pbar = fill * filled_length + zfill * (length - filled_length)
-    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
+    return "{0} |{1}| {2}% {3}".format(prefix, pbar, percent, suffix)
 
 
 def notify_progress(seconds_left, total, chat_id, message_id, bot):
     progress_bar = render_progressbar(total, seconds_left)
     message = f"Осталось секунд: {seconds_left}\n{progress_bar}"
     bot.update_message(chat_id, message_id, message)
-    if (seconds_left == 0):
+    if seconds_left == 0:
         bot.send_message(chat_id, "Время вышло")
 
 
@@ -30,12 +30,12 @@ def start_notify_progress(chat_id, message, bot):
     message = f"Запускаю таймер на {seconds} сек."
     message_id = bot.send_message(chat_id, message)
     bot.create_countdown(
-      seconds,
-      notify_progress,
-      total=seconds,
-      chat_id=chat_id,
-      message_id=message_id,
-      bot=bot
+        seconds,
+        notify_progress,
+        total=seconds,
+        chat_id=chat_id,
+        message_id=message_id,
+        bot=bot,
     )
 
 
